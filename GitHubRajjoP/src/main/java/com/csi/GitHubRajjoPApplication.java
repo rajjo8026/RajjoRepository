9f090bcc6a2cfbbe75d0a62d6aa9028815227910
rajjo8026 <ranjitb8026@gmail.com>
2022-08-24 15:31:37 +0530

    Commited by ranjit

---
+++ GitHubRajjoP/src/main/java/com/csi/GitHubRajjoPApplication.java
@@ -0,0 +1,13 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubRajjoPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubRajjoPApplication.class, args);
+	}
+
+}
